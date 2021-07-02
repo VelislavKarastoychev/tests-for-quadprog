187c0c991a6ab87bda538de8fc57d3ca4efd3835
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-02 11:38:08 +0300

    The conventional javascript test.

---
+++ Tests/AlbertoSantiniMatrix.js
@@ -0,0 +1,36 @@
+'use strict';
+let Dmat = [], dvec = [], Amat = [], bvec = [], res;
+
+Dmat[0] = [];
+Dmat[1] = [];
+Dmat[2] = [];
+Dmat[0][0] = 1;
+Dmat[1][0] = 0;
+Dmat[2][0] = 0;
+Dmat[0][1] = 0;
+Dmat[1][1] = 1;
+Dmat[2][1] = 0;
+Dmat[0][2] = 0;
+Dmat[1][2] = 0;
+Dmat[2][2] = 1;
+
+dvec[0] = 0;
+dvec[1] = 5;
+dvec[2] = 0;
+
+Amat[0] = [];
+Amat[1] = [];
+Amat[2] = [];
+Amat[0][0] = -4;
+Amat[1][0] = -3;
+Amat[2][0] = 0;
+Amat[0][1] = 2;
+Amat[1][1] = 1;
+Amat[2][1] = 0;
+Amat[0][2] = 0;
+Amat[1][2] = -2;
+Amat[2][2] = 1;
+
+bvec[0] = -8;
+bvec[1] = 2;
+bvec[2] = 0;

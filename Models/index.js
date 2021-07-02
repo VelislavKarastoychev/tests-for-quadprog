99d1c2b4c77e272d9702367205442de7ebcef7cc
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-02 22:57:45 +0300

    Collect all models.

---
+++ Models/index.js
@@ -0,0 +1,9 @@
+'use strict';
+const transformToUnitBase = require('./copyInUnitBase');
+const AreSimilar = require('.//AreSimilar')
+const example_label = require('./exampleLabel');
+module.exports = {
+    transformToUnitBase,
+    AreSimilar,
+    example_label,
+};
